15312963dfd97cf99f314e734e2849d5954d4ceb
Jacob Hurst <Jhurst@unm.edu>
2018-09-23 12:31:26 -0600

    Update to look/feel of GUI

--- TrafficSimulator/src/Primary/Main.java
+++ TrafficSimulator/src/Primary/Main.java
@@ -6,9 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -25,38 +28,45 @@ public class Main extends Application {
         // Setup border pane with HBox of two buttons along the top and
         // canvas to display the simulation in the center, also initialize
         // the controller with something to draw on
-
         BorderPane root = new BorderPane();
         Canvas canvas = new Canvas(550, 550);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        HBox buttonPane = new HBox(30);
-        Button carButton = new Button("New Car");
-        Button clearButton = new Button("Clear Traffic");
+
+        VBox controlBox = new VBox(15);
+        controlBox.setPrefSize(150, 550);
+        controlBox.setStyle("-fx-background-color: #e0e0e0;");
+
+        Label controlLabel = new Label("Modes:");
+
+        Button heavyButton = new Button("Heavy Traffic");
+        Button moderateButton = new Button("Moderate Traffic");
+        Button lightButton = new Button("Light Traffic");
+        Button combinationButton = new Button("Combination Traffic");
+        Button spawnButton = new Button("Spawn");
+        Button resetButton = new Button("Reset");
 
         Controller controller = new Controller(gc);
         controller.start();
 
         // Handle button press actions
-
-        carButton.setOnMousePressed(e -> {
+        spawnButton.setOnMousePressed(e -> {
             controller.spawnCar();
         });
-
-        clearButton.setOnMousePressed(e -> {
+        resetButton.setOnMousePressed(e -> {
             controller.clearTraffic();
         });
 
 
         // Setup the scene
+        controlBox.getChildren().addAll(controlLabel, heavyButton, moderateButton, lightButton,
+                                        combinationButton, spawnButton, resetButton);
+        root.setRight(controlBox);
+        root.setLeft(canvas);
 
-        buttonPane.getChildren().addAll(carButton,clearButton);
-        root.setTop(buttonPane);
-        root.setCenter(canvas);
-
-        Scene scene = new Scene(root, 550, 550);
+        Scene scene = new Scene(root, 700, 550);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Traffic Simulator");
+        primaryStage.setTitle("Traffic Control System: Testbed");
         primaryStage.show();
     }
 
